eec4b76d37f00c40be6dab610f5aeefc44e6f549
ItsEonPE <31456550+ItsEonPE@users.noreply.github.com>
2017-12-09 11:19:57 -0800

    Update main.php

--- src/eon/JoinWelcome/main.php
+++ src/eon/JoinWelcome/main.php
@@ -1,3 +1,15 @@
+<?php
+
+namespace eon;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use picketmine\Player;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\utils\TextFormat as C;
+
+class JoinMessage extends PluginBase implements Listener {
 
 public $prefix = C::GRAY."[".C::GOLD."JoinWelcome".C::GRAY."]";
          //[JoinWelcome]
