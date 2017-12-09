c72fa153705ff3edab439adfbd992d88a5f35cdb
ItsEonPE <31456550+ItsEonPE@users.noreply.github.com>
2017-12-09 11:00:47 -0800

    added basics

--- src/eon/JoinWelcome/main.php
+++ src/eon/JoinWelcome/main.php
@@ -1,1 +1,12 @@
+<?php
 
+namespace eon;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use picketmine\Player;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\utils\TextFormat as C;
+
+class JoinWelcome extends PluginBase implements Listener {
