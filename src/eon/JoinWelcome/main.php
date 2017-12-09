b788da13c4372cd37db267b5e8f50d6751ddb5cd
ItsEonPE <31456550+ItsEonPE@users.noreply.github.com>
2017-12-09 11:18:52 -0800

    Update main.php

--- src/eon/JoinWelcome/main.php
+++ src/eon/JoinWelcome/main.php
@@ -1,12 +1,40 @@
-<?php
 
-namespace eon;
+public $prefix = C::GRAY."[".C::GOLD."JoinWelcome".C::GRAY."]";
+         //[JoinWelcome]
 
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
-use picketmine\Player;
-use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\utils\TextFormat as C;
+public function onEnable(){
 
-class JoinWelcome extends PluginBase implements Listener {
+          $this->getServer()->getPluginManager()->registerEvents($this, $this);
+          $this->getlogger()->Info($this->prefix . C::RED . "Is Active");
+         
+          }
+
+public function onDisable(){
+
+          $this->getLogger()->Info($this->prefix . C::BLUE . " Is Disabled");
+          
+          }
+
+public function onJoin(PlayerJoinEvent $event){
+
+            $player = $event->getPlayer();
+            
+            if ($player->isOp()){
+                      $event->setJoinMessage(C::GRAY . "[". C::DARK_RED . "-" . C::GRAY . "]" .C::DARK_GRAY . C::BOLD . " » " .C::RESET . C::GREEN . $player->getName() . C::GRAY ." joined the game");
+                            } else {
+                              $event->setJoinMessage(C::GRAY . "[". C::DARK_RED . "-" . C::GRAY . "]" .C::DARK_GRAY . C::BOLD . " » " .C::RESET . C::GREEN . $player->getName() . C::GRAY ." joined the game");
+                              }
+                            }
+                             
+public function onQuit(PlayerQuitEvent $event){
+
+              $player = $event->getPlayer();
+              
+              if ($player->isOp()){
+                     $event->setQuitMessage(C::GRAY . "[". C::DARK_RED . "-" . C::GRAY . "]" .C::DARK_GRAY . C::BOLD . " » " .C::RESET . C::GREEN . $player->getName() . C::GRAY ." has left the game");
+                       }else{
+                           $event->setQuitMessage(C::GRAY . "[". C::DARK_RED . "-" . C::GRAY . "]" .C::DARK_GRAY . C::BOLD . " » " .C::RESET . C::GREEN . $player->getName() . C::GRAY ." has left the game");
+                           }
+                         }
+
+}  
